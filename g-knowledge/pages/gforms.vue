ac8b8bc251e60d5b876eb932727bd2f502e42876
Dan <dmalengela@yahoo.com>
2023-07-11 14:55:04 +0200

    created gforms.vue

---
+++ g-knowledge/pages/gforms.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>GForms</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, fugit officiis maiores, 
+        reiciendis earum eligendi laborum delectus tempore nesciunt blanditiis alias. 
+        Error enim voluptatum exercitationem magnam consequuntur impedit ipsam in!
+      </p>
+    </div>
+  </template>
+  
+  <script></script>
+  
+  <style>
+  h1{
+    color: skyblue;
+    text-align: center;
+  }
+  </style>
+  
