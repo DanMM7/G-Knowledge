25f9c0228e4477d627b0ed723a23f2f3b2fdc778
Dan <dmalengela@yahoo.com>
2023-07-11 14:54:51 +0200

    Created index.vue

---
+++ g-knowledge/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Helloworld!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iusto distinctio expedita porro aut explicabo vitae omnis quas, 
+        quo maxime! Rerum debitis asperiores quo cupiditate deleniti, reiciendis alias non nihil.
+      </p>
+    </div>
+  </template>
+  
+  <script></script>
+  
+  <style>
+  h1{
+    color: skyblue;
+    text-align: center;
+  }
+  </style>
+  
